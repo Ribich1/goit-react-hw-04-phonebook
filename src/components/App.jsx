096afaf4533972e5c586be6c63e987aa0626588e
Ribich1 <ribich@ukr.net>
2023-08-14 02:43:06 +0200

    hooks2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -60,21 +60,7 @@ export default function App() {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-  useEffect(() => {
-    console.log('useeffect');
-    const contactsFromLs = localStorage.getItem('contacts');
-    const parseContactsFromLs = JSON.parse(contactsFromLs);
-    console.log('parseContactsFromLs', parseContactsFromLs);
-    if (parseContactsFromLs && parseContactsFromLs.length !== 0) {
-      setContacts(parseContactsFromLs);
-    } else
-      setContacts([
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]);
-  }, []);
+
 
   return (
     <>
